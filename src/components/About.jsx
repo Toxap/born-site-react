19b98f4fc828667794d831948b7b919ebf710df7
Toxap <62807389+Toxap@users.noreply.github.com>
2025-06-15 15:14:05 +0700

    Add ScrollReveal animation for About description

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,7 @@
 import CardSwap, { Card } from './CardSwap.jsx'
 import ParticleBackground from './ParticleBackground.jsx'
 import ScrollFloat from './ScrollFloat.jsx'
+import ScrollReveal from './ScrollReveal.jsx'
 import './About.css'
 
 export default function About() {
@@ -10,12 +11,12 @@ export default function About() {
       <ScrollFloat containerClassName="about-title">
         О наших курсах
       </ScrollFloat>
-      <p className="about-description">
-        Наша школа программирования предоставляет современное образование, 
+      <ScrollReveal containerClassName="about-description">
+        Наша школа программирования предоставляет современное образование,
         основанное на практике и работе с актуальными технологиями. Мы создаём
-        пространство, где каждый может раскрыть свой потенциал и получить 
+        пространство, где каждый может раскрыть свой потенциал и получить
         поддержку опытных наставников.
-      </p>
+      </ScrollReveal>
       <CardSwap width={700} height={400} cardDistance={50} verticalDistance={60}>
         <Card customClass="about-card about-card-1" />
         <Card customClass="about-card about-card-2" />
